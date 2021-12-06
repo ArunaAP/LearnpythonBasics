bd48343624513b2b92c8069aa100f1ba98e20455
Aruna priyankara <90405801+ArunaAP@users.noreply.github.com>
2021-12-07 00:08:52 +0530

    1rt commit Tuples.py

---
+++ Tuples.py
@@ -0,0 +1,9 @@
+coordinates = (5, 4)
+#coordinates[1] = 10  #can't change
+print(coordinates[1])
+#Tuples are immuterble (can not change)
+
+
+
+coordinates1 = [(5, 4), (10, 20), (25, 36), (55, 41)]
+print(coordinates1[2])

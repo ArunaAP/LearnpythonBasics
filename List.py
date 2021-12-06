83e289d86c49cac61eddf550031ac10ca8907671
Aruna priyankara <90405801+ArunaAP@users.noreply.github.com>
2021-12-06 20:59:42 +0530

    1st List commit

---
+++ List.py
@@ -0,0 +1,8 @@
+
+friends = ["Kevin", "Lara", "Jim", "Tom", "Toby"]
+#             0        1       2     3       4
+print(friends)
+print(friends[1])
+print(friends[1:]) #print after 1
+print(friends[1:3]) # print 1-3
+

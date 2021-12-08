48169e13406b08ce5ba494861bd7d6eb1f8be0c3
Aruna priyankara <90405801+ArunaAP@users.noreply.github.com>
2021-12-08 12:20:38 +0530

    2st commit functions.py(return statement)

--- functions.py
+++ functions.py
@@ -1,6 +1,17 @@
+#functions
 def say_hi(name):
     print("Hello " + name)
 
 
 say_hi("Mike")
 say_hi("Lara")
+
+
+#return statement
+
+def cube(num):
+    return num * num * num
+
+
+result = cube(2)
+print(result)

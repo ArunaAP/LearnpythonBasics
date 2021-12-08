c10b59241b094fa6efbc8bd1a9ebb72027088812
Aruna priyankara <90405801+ArunaAP@users.noreply.github.com>
2021-12-08 14:10:51 +0530

    commit Dictionaries.py

---
+++ Dictionaries.py
@@ -0,0 +1,18 @@
+
+monthConcersion = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "Octomber",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+print(monthConcersion["Nov"])
+print(monthConcersion.get("Des"))

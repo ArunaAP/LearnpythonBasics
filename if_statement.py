e369d42279b6e0839030b85a61003f454117f707
Aruna priyankara <90405801+ArunaAP@users.noreply.github.com>
2021-12-08 13:30:48 +0530

    2nd commit if_statement.py

--- if_statement.py
+++ if_statement.py
@@ -9,3 +9,17 @@ elif not(is_male) and is_tall:
     print("You are not a male but are tall")
 else:
     print("You are neither not male not tall or both")
+
+
+#if statement & comparosons
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >=num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+
+print(max_num(3,4,5))
+

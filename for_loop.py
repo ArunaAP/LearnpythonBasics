21e267ceee80a6a1b3bdeb58d341bfa18c30e0d9
Aruna priyankara <90405801+ArunaAP@users.noreply.github.com>
2021-12-09 14:08:27 +0530

    commit for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,15 @@
+
+for letter in "Giraffe":
+    print(letter)
+
+
+friends = ["Kevin", "Lara", "Jim", "Tom", "Toby"]
+for friends in friends:
+     print(friends)
+
+for index in range(10): #range(3, 10):
+    print(index)
+
+friends = ["Kevin", "Lara", "Jim", "Tom", "Toby"]
+for index in range(len(friends)):
+     print(friends[index])

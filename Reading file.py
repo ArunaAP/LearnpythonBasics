35c55be4f5577496c050c4d04895203d46fb255b
Aruna priyankara <90405801+ArunaAP@users.noreply.github.com>
2021-12-17 20:01:38 +0530

    commit Reading file.py

---
+++ Reading file.py
@@ -0,0 +1,19 @@
+
+employee_file = open("employee.txt", "r") # r - read / w - write / a - append(can modified any info) / r+ - read & write
+
+print(employee_file.readable()) #true or false
+#print(employee_file.read())
+
+
+'''
+print(employee_file.readline()) #print line
+print(employee_file.readline()) # by line
+
+print(employee_file.readlines()) # print like list 
+'''
+
+#using for loop
+for employee in employee_file.readlines():
+    print(employee)
+
+employee_file.close()
